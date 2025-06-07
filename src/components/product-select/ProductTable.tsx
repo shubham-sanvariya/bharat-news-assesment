4ea85e6a725cbd70f061860f366966f594471513
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-07 15:06:37 +0530

    Refactor ProductTable component to utilize SelectableTable for improved product selection and data rendering, while updating type definitions to support multiple selected products.

--- src/components/product-select/ProductTable.tsx
+++ src/components/product-select/ProductTable.tsx
@@ -1,82 +1,39 @@
 import React, {useEffect} from 'react';
-import { Table } from 'antd';
 import type {Product} from "../../types/models.ts";
+import SelectableTable from "../table/SelectableTable.tsx";
+import {productsColumns, productsData} from "../../utils/mockData.ts";
 
 interface ProductTableProps {
-    selectedProduct: Product | null;
-    setSelectedProduct: (product: Product) => void;
+    selectedProduct: Product[] | null;
+    setSelectedProduct: (product: Product[]) => void;
 }
 
 const ProductTable: React.FC<ProductTableProps> = ({
                                                        selectedProduct,
                                                        setSelectedProduct
                                                    }) => {
-    const columns = [
-        {
-            title: 'Product Name',
-            dataIndex: 'productName',
-            key: 'productName',
-        },
-        {
-            title: 'SBOM Name',
-            dataIndex: 'sbomName',
-            key: 'sbomName',
-        },
-        {
-            title: 'Version',
-            dataIndex: 'version',
-            key: 'version',
-        },
-        {
-            title: 'SKU ID',
-            dataIndex: 'skuId',
-            key: 'skuId',
-        },
-        {
-            title: 'Modified Date',
-            dataIndex: 'modifiedDate',
-            key: 'modifiedDate',
-        },
-    ];
 
-    const data: Product[] = [
-        {
-            key: '1',
-            productName: 'Acme Switch',
-            sbomName: 'Acme Switch',
-            version: '2.2.5',
-            skuId: '18',
-            modifiedDate: 'May 05, 2025 08:24 AM',
-        },{
-            key: '2',
-            productName: 'red Switch',
-            sbomName: 'red Switch',
-            version: '3.2.5',
-            skuId: '25',
-            modifiedDate: 'May 03, 2025 09:24 AM',
-        },
-        // Add more products as needed
-    ];
 
     useEffect(() => {
         console.log(selectedProduct)
     }, [selectedProduct]);
 
     return (
-        <Table
-            columns={columns}
-            dataSource={data}
-            rowSelection={{
-                type: 'radio',
-                selectedRowKeys: selectedProduct ? [selectedProduct.key] : [],
-                onChange: (_, selectedRows) => {
-                    setSelectedProduct(selectedRows[0]);
-                },
-            }}
-            rowKey="key"
-            pagination={false}
-            className="product-table"
-        />
+        // <Table
+        //     columns={columns}
+        //     dataSource={data}
+        //     rowSelection={{
+        //         type: 'radio',
+        //         selectedRowKeys: selectedProduct ? [selectedProduct.key] : [],
+        //         onChange: (_, selectedRows) => {
+        //             setSelectedProduct(selectedRows[0]);
+        //         },
+        //     }}
+        //     rowKey="key"
+        //     pagination={false}
+        //     className="product-table"
+        // />
+        <SelectableTable columns={productsColumns} data={productsData} selectedData={selectedProduct} setSelectedData={setSelectedProduct} className={"product-table"}/>
     );
 };
 
