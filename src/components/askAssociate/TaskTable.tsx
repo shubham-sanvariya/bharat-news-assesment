bcacac6e1c7c4e79c1145edf0638be5bf9f107e1
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-07 15:05:39 +0530

    Refactor TaskTable component to utilize SelectableTable with mock data and column definitions for improved structure and maintainability

--- src/components/askAssociate/TaskTable.tsx
+++ src/components/askAssociate/TaskTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Table } from 'antd';
 import type {Task} from "../../types/models.ts";
+import SelectableTable from "../table/SelectableTable.tsx";
+import {taskColumns, taskData} from "../../utils/taskMockData.ts";
 
 interface TaskTableProps {
     selectedTask: Task | null;
@@ -11,68 +12,13 @@ const TaskTable: React.FC<TaskTableProps> = ({
                                                  selectedTask,
                                                  setSelectedTask
                                              }) => {
-    const columns = [
-        {
-            title: 'Task Name',
-            dataIndex: 'taskName',
-            key: 'taskName',
-        },
-        {
-            title: 'Vendor Name',
-            dataIndex: 'vendorName',
-            key: 'vendorName',
-        },
-        {
-            title: 'Status',
-            dataIndex: 'status',
-            key: 'status',
-        },
-        {
-            title: 'Modified Date',
-            dataIndex: 'modifiedDate',
-            key: 'modifiedDate',
-        },
-    ];
-
-    const data: Task[] = [
-        {
-            key: '1',
-            taskName: 'SBOM Request received for product Acme Switch - 2.2.5',
-            vendorName: 'Quick Solutions',
-            status: 'Pending',
-            modifiedDate: 'May 05, 2025 08:24 AM',
-        },
-        {
-            key: '2',
-            taskName: 'SBOM Request received for product caddy:test_hp4d1',
-            vendorName: 'NewAge Software',
-            status: 'Pending',
-            modifiedDate: 'Apr 28, 2025 08:24 AM',
-        },
-        {
-            key: '3',
-            taskName: 'SBOM Request received for product apache-avro:AVRO-3860',
-            vendorName: 'SeeToo Inc',
-            status: 'Pending',
-            modifiedDate: 'Apr 21, 2025 08:24 AM',
-        },
-    ];
 
     return (
-        <Table
-            columns={columns}
-            dataSource={data}
-            rowSelection={{
-                type: 'radio',
-                selectedRowKeys: selectedTask ? [selectedTask.key] : [],
-                onChange: (_, selectedRows) => {
-                    setSelectedTask(selectedRows[0]);
-                },
-            }}
-            rowKey="key"
-            pagination={false}
-            className="task-table"
-        />
+        <SelectableTable columns={taskColumns}
+                         data={taskData}
+                         selectedData={selectedTask}
+                         setSelectedData={setSelectedTask}
+                         className={"task-table"}/>
     );
 };
 
