56f18bd2786dda55ca1bc8871016eac934e85f68
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-07 15:42:42 +0530

    Refactor VendorTable component by removing unused action buttons and row selection logic, streamlining the layout to focus on the SelectableTable for vendor selection.

--- src/components/VendorSelect/VendorTable.tsx
+++ src/components/VendorSelect/VendorTable.tsx
@@ -1,5 +1,4 @@
 import React, {useMemo} from 'react';
-import { Button } from 'antd';
 import type {Vendor} from "../../types/models.ts";
 import SelectableTable from "../table/SelectableTable.tsx";
 import {vendorColumns, vendorData} from "../../utils/vendorMockData.ts";
@@ -28,35 +27,9 @@ const VendorTable: React.FC<VendorTableProps> = ({
         vendor.primaryEmail.toLowerCase().includes(searchText.toLowerCase())
     ),[searchText]);
 
-    // const rowSelection = {
-    //     selectedRowKeys: selectedVendors.map(vendor => vendor.key),
-    //     onChange: (_selectedRowKeys: React.Key[], selectedRows: Vendor[]) => {
-    //         setSelectedVendors(selectedRows);
-    //     },
-    // };
-
     return (
         <div className="vendor-table-container">
-            <div className="table-actions">
-                <Button type="primary" className="add-vendor-btn">
-                    Add Vendor
-                </Button>
-                <Button danger disabled={selectedVendors.length === 0}>
-                    Delete
-                </Button>
-            </div>
 
-            {/*<Table*/}
-            {/*    columns={columns}*/}
-            {/*    dataSource={filteredData}*/}
-            {/*    rowSelection={{*/}
-            {/*        type: 'checkbox',*/}
-            {/*        ...rowSelection,*/}
-            {/*    }}*/}
-            {/*    rowKey="key"*/}
-            {/*    pagination={false}*/}
-            {/*    className="vendor-table"*/}
-            {/*/>*/}
             <SelectableTable columns={vendorColumns} data={filteredData} selectedData={selectedVendors} setSelectedData={setSelectedVendors} className={"vendor-table"}/>
         </div>
     );
